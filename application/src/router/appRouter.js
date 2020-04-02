1949900896e4e50ebfa494582fc87199dc74178f
Marco <marcode.chavez.jr@gmail.com>
2020-04-02 12:16:40 -0700

    feat(auth navigating): redirecting for non auth users
    added the functionality to redirect users who don't have an auth to the login page.

--- application/src/router/appRouter.js
+++ application/src/router/appRouter.js
@@ -1,16 +1,37 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { Main, Login, OrderForm, ViewOrders } from '../components';
 
+const PrivateRoute = ({ Component, auth, ...rest }) => (
+	<Route
+		{...rest}
+		render={props => {
+			if (auth.token != null) {
+				return <Component {...props} />;
+			} else {
+				return <Redirect to="/login" />;
+			}
+		}}
+	/>
+);
+
 const AppRouter = (props) => {
+
+  const { auth } = props;
+
   return (
     <Router>
       <Route path="/" exact component={Main} />
       <Route path="/login" exact component={Login} />
-      <Route path="/order" exact component={OrderForm} />
-      <Route path="/view-orders" exact component={ViewOrders} />
+      <PrivateRoute path="/order" exact Component={OrderForm} auth={auth} />
+      <PrivateRoute path="/view-orders" exact Component={ViewOrders} auth={auth} />
     </Router>
   );
 }
 
-export default AppRouter;
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+})
+
+export default connect(mapStateToProps, null)(AppRouter);
